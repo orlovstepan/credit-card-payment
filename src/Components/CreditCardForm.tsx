4bba945c4fb1d77cb03c7272aa15111b82d3055a
orlovstepan <orlovst94@gmail.com>
2023-04-23 22:52:50 +0200

    has some bugs but working

--- src/Components/CreditCardForm.tsx
+++ src/Components/CreditCardForm.tsx
@@ -26,10 +26,27 @@ export default function CreditCardForm() {
   const [details, setDetails] = useState<typeof initialState>(initialState);
 
   const validateCardNumber = (str: string) => {
-    return str.replace(/\D/, '')
+    const cardNumber = str.replace(/\D/, '');
+    const formattedCardNumber = cardNumber.replace(/(\d{4})(?=\d)/g, '$1 ');
+    // return formattedCardNumber;
+    // let formattedCardNumber = '';
+    // for (let i=0;i<str.length;i++){
+    //   if(i%4===0){
+    //     // formattedCardNumber += ""
+    //     console.log("NOW")
+    //   }
+    //   formattedCardNumber += str[i]
+    // }
+    return formattedCardNumber;
   }
   const validateName = (str: string) => {
     return str.replace(/[0-9]/, '')
+  }
+
+  const validateExpiry = (str: string) => {
+    const date = str.replace(/\D/, '');
+    const formattedDate = date.replace(/(\d{2})(?=\d)/g, "$1/")
+    return formattedDate
   }
 
   const handleChange = (e: any) => {
@@ -43,6 +60,9 @@ export default function CreditCardForm() {
     }
     if (field === "CVC") {
       value = validateCardNumber(value);
+    }
+    if (field === 'expiryDate'){
+      value = validateExpiry(value)
     }
     setDetails({
       ...details,
@@ -63,16 +83,16 @@ export default function CreditCardForm() {
       <form className={styles['form']} onChange={handleChange}>
         <div className= {styles["name"]}>
           <label htmlFor="name" > Full Name </label>
-          <input type='text' placeholder="Alex Smith" id="name" value={details.name.value} />
+          <input type='text' placeholder="ALEX SMITH" id="name" value={details.name.value} />
         </div>
         <div className={styles["card-number"]}>
           <label htmlFor="cardNumber"> Card Number </label>
-          <input type="text" id="cardNumber" placeholder="1234 1234 1234 1234" maxLength={16} value={details.cardNumber.value} />
+          <input type="text" id="cardNumber" placeholder="1234 1234 1234 1234" maxLength={19} value={details.cardNumber.value} />
         </div>
         <div className={styles["date-cvc"]}>
           <div className={styles["date"]}>
-            <label> Valid till </label>
-            <input placeholder="MM/YY"></input>
+            <label htmlFor="expiryDate"> Valid till </label>
+            <input maxLength={5} id="expiryDate" placeholder="MM/YY" value={details.expiryDate.value}></input>
           </div>
           <div className={styles["cvc"]}>
             <label htmlFor="CVC"> CVC </label>
